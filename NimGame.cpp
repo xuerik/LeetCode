052881896dce818cb0cb7c1da322e9fb06f01422
Erik Xu <xuerik@users.noreply.github.com>
2015-10-15 23:57:25 -0700

    Create NimGame.cpp

---
+++ NimGame.cpp
@@ -0,0 +1,16 @@
+/* Erik Xu
+*  10/15/15
+*  URL: https://leetcode.com/submissions/detail/43141119/
+*/
+
+class Solution {
+    
+public:
+    bool canWinNim(int n) {
+        if(n % 4 == 0)
+            return false;
+            
+        else
+            return true;
+    }
+};
